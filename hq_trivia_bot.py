0aa13a88120b8cd261ab1490b7a9539b7ff5c714
asitpati911 <78376360+asitpati911@users.noreply.github.com>
2023-06-16 19:26:20 +0530

    Create hq_trivia_bot.py

---
+++ hq_trivia_bot.py
@@ -0,0 +1,72 @@
+import discord
+import requests
+import asyncio
+import openai
+
+# Set up the Discord client
+client = discord.Client()
+
+# Set up the OpenAI API
+openai.api_key = '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'
+
+# Set up the Google search URL
+GOOGLE_SEARCH_URL = 'https://www.google.com/search?q='
+
+# Replace 'YOUR_DISCORD_TOKEN' with your actual Discord bot token
+DISCORD_TOKEN = 'MTExODQ2MjYxMDQ2ODI0MTQyOA.GyoN5F.SKcg2Z6FPEXhjiu0FdKqRFWY3Azun1a9vN_zSA'
+
+@client.event
+async def on_ready():
+    print(f'Logged in as {client.user.name}')
+
+@client.event
+async def on_message(message):
+    # Ignore messages sent by the bot itself
+    if message.author == client.user:
+        return
+
+    # Check if the message starts with the command prefix
+    if message.content.startswith('!hq'):
+        # Extract the question from the message content
+        question = message.content[4:]
+
+        # Perform a Google search to get the answer
+        answer = get_google_answer(question)
+
+        # Generate an answer using ChatGPT
+        chatgpt_answer = get_chatgpt_answer(question)
+
+        # Send the answer to the Discord channel
+        await message.channel.send(f'Google Search Answer: {answer}')
+        await message.channel.send(f'ChatGPT Answer: {chatgpt_answer}')
+
+def get_google_answer(question):
+    search_query = f'{question} site:wikipedia.org'
+    search_url = GOOGLE_SEARCH_URL + search_query
+    response = requests.get(search_url)
+    response.raise_for_status()
+    search_results = response.text
+    # Extract the answer from the search results using parsing or scraping techniques
+    answer = parse_answer_from_search_results(search_results)
+    return answer
+
+def get_chatgpt_answer(question):
+    prompt = f'What is the answer to the question: {question}?'
+    response = openai.Completion.create(
+        model='gpt-3.5-turbo',
+        prompt=prompt,
+        max_tokens=50,
+        n=1,
+        stop=None,
+        temperature=0.7
+    )
+    chatgpt_answer = response.choices[0].text.strip()
+    return chatgpt_answer
+
+def parse_answer_from_search_results(search_results):
+    # Implement your own parsing or scraping logic here to extract the answer from the search results
+    # This will depend on the structure of the search results page
+    return "Answer"
+
+# Run the Discord client
+client.run(DISCORD_TOKEN)
